f5e1988d6f8e54dc0212e8343102a3b98b05fbc0
naomi-hidaka <naomi-hidaka@cookpad.com>
2017-04-26 14:37:26 +0900

    Init

---
+++ testSalad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testSalad
+//
+//  Created by naomi-hidaka on 2017/04/26.
+//  Copyright © 2017年 naomi-hidaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
